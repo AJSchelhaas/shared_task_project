7b09220ffd10cf431f5f4f4122bb733caa3827f8
JMvdHeide <jmvdheide96@gmail.com>
2020-12-09 13:13:24 +0100

    jobscript for finetuning on peregrine

---
+++ finetuning/finetuning_job.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=BERT_Finetuning
+#SBATCH --time=1:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=4000
+#SBATCH --output=../logs/Peregrine/job-%j.log
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:v100:1
+#SBATCH --mail-user=jmvdheide96@gmail.com
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+module load Python
+source VirtualEnv/bin/activate
+pip install --upgrade pip
+pip install transformers
+pip install datasets
+
+python run_mlm.py \
+    --model_name_or_path ../model/toxic_classifier.model \
+    --train_file ../Data/converted_data_train.conll \
+    --validation_file ..Data/converted_data_dev.conll \
+    --do_train \
+    --do_eval \
+    --output_dir /tmp/mlm
